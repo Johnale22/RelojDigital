ebc4770dff5b20415bc10299c554b602a8527793
JohnAle2207 <jaguerrac@uce.edu.ec>
2025-05-25 23:06:58 -0500

    actions3

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,8 +11,10 @@ def home():
     return render_template('index.html', visits=count)
 
 if __name__ == '__main__':
+    # Inicializa el contador solo si el archivo está vacío
     with open("counter.txt", "a+") as f:
         f.seek(0)
         if not f.read():
             f.write("0")
+    # Ejecuta el servidor Flask solo en modo desarrollo
     app.run(debug=True)
